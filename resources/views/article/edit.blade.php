205f41f6abbe181b2074c07eb46785e0fd73b9b6
mlataza <1750947+mlataza@users.noreply.github.com>
2024-10-01 21:08:25 +0800

    fix select input comparison

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -55,7 +55,7 @@
                                 <select id="edit_article_company" name="company_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     <option value="">Select company...</option>
                                     @foreach ($companies as $company)
-                                    <option @if(old('company_id', $article->company_id) === $company->id) selected @endif value="{{ $company->id }}">{{ $company->name }}</option>
+                                    <option @if(old('company_id', $article->company_id) == $company->id) selected @endif value="{{ $company->id }}">{{ $company->name }}</option>
                                     @endforeach
                                 </select>
                                 <x-input-error :messages="$errors->get('company_id')" class="mt-2" />
